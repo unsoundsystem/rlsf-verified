c695ac494ca61aa8eedcdbe186321fa8fd8a3438
unsoundsystem <unsoundsystem@protonmail.com>
2025-01-16 23:38:11 +0900

    rlsf-verified: proved some properties about rational numbers

--- rlsf-verified/src/rational_numbers.rs
+++ rlsf-verified/src/rational_numbers.rs
@@ -1,8 +1,8 @@
 use vstd::prelude::*;
-use vstd::relations::{equivalence_relation, injective, transitive, connected};
-use vstd::arithmetic::mul::{lemma_mul_equality_converse, lemma_mul_nonzero};
+use vstd::relations::{equivalence_relation, injective, transitive};
+use vstd::arithmetic::mul::{lemma_mul_is_commutative, lemma_mul_strict_inequality, lemma_mul_equality_converse, lemma_mul_nonzero};
 use vstd::calc;
-use crate::relation_utils::{strict_total_ordering};
+use crate::relation_utils::{strict_total_ordering, connected};
 
 verus! {
 /// Rational number `num/den`
@@ -31,6 +31,10 @@ impl Rational {
 
     spec fn lt(self, rhs: Self) -> bool {
         self.num * rhs.den < rhs.num * self.den
+    }
+
+    spec fn lte(self, rhs: Self) -> bool {
+        self.lt(rhs) || self.eq(rhs)
     }
 
     spec fn eq_int(self, rhs: int) -> bool {
@@ -100,23 +104,74 @@ impl Rational {
     {}
 
     proof fn lemma_lt_is_connected()
-        ensures connected(|p: Self, q: Self| p.lt(q))
-    {
+        ensures connected(|p: Self, q: Self| p.lt(q), |p: Self, q: Self| p.eq(q))
+    {}
 
+    proof fn lemma_lt_is_transitive()
+        ensures transitive(|p: Self, q: Self| p.lt(q))
+    {
+        assert forall |p: Self, q: Self, r: Self| p.lt(q) && q.lt(r) implies p.lt(r) by {
+            calc! {
+                (==>)
+                p.lt(q); { lemma_mul_is_commutative(q.num, p.den); assert(p.lt(q) ==> p.num * q.den < p.den * q.num) by (compute) }
+                p.num * q.den < p.den * q.num; {
+                    r.axiom_denominator_is_nonzero();
+                    lemma_mul_equality_converse_right(p.num * q.den, p.den * q.num, r.den);
+                }
+                p.num * q.den * r.den < p.den * q.num * r.den;
+            };
+
+            calc! {
+                (==>)
+                q.lt(r); { lemma_mul_is_commutative(r.num, q.den); assert(q.lt(r) ==> q.num * r.den < q.den * r.num) by (compute) }
+                q.num * r.den < q.den * r.num; {
+                    p.axiom_denominator_is_nonzero();
+                    lemma_mul_strict_inequality_imp(q.num * r.den, q.den * r.num, p.den);
+                    vstd::arithmetic::mul::lemma_mul_is_associative(p.den, q.num, r.den);
+                    vstd::arithmetic::mul::lemma_mul_is_associative(p.den, q.den, r.num);
+                }
+                p.den * q.num * r.den < p.den * q.den * r.num;
+            }
+
+            // transitivity of inequality on int
+            assert((p.num * q.den * r.den < p.den * q.num * r.den) &&
+                (p.den * q.num * r.den < p.den * q.den * r.num)
+                ==> p.num * q.den * r.den < p.den * q.den * r.num) by (nonlinear_arith);
+
+            q.axiom_denominator_is_nonzero();
+            assert(p.num * q.den * r.den < p.den * q.den * r.num ==> p.num * r.den * q.den < p.den * r.num * q.den) by (nonlinear_arith);
+            lemma_mul_strict_inequality_converse_imp(p.num * r.den, p.den * r.num, q.den);
+            assert(p.num * r.den * q.den < p.den * r.num * q.den ==> p.num * r.den < p.den * r.num);
+            vstd::arithmetic::mul::lemma_mul_is_commutative(r.num, p.den);
+            assert(p.num * r.den < r.num * p.den ==> p.lt(r));
+        }
     }
 
-    //proof fn lemma_lt_is_strict_total()
-        //ensures strict_total_ordering(|p: Self, q: Self| p.lt(q))
-    //{}
+    proof fn lemma_lt_is_strict_total()
+        ensures strict_total_ordering(|p: Self, q: Self| p.lt(q), |p: Self, q: Self| p.eq(q))
+    {
+        Self::lemma_equivalence(); // to surpress recommendation warning
+        Self::lemma_lt_is_transitive();
+    }
 }
-
-proof fn lemma_mul_equality(a: int, b: int, c: int, d: int)
-    ensures
-        a == b && c == d ==> a*c == b*d
-{}
 
 proof fn lemma_mul_zero_choose(x: int, y: int) by (nonlinear_arith)
     ensures x*y == 0 ==> x == 0 || y == 0
+{}
+
+proof fn lemma_mul_equality_converse_right(x: int, y: int, z: int) by (nonlinear_arith)
+    ensures
+        z > 0 && x < y ==> x * z < y * z
+{}
+
+proof fn lemma_mul_strict_inequality_converse_imp(x: int, y: int, z: int) by (nonlinear_arith)
+    ensures
+        #[trigger] (x * z) < #[trigger] (y * z) && z >= 0 ==> x < y
+{}
+
+proof fn lemma_mul_strict_inequality_imp(x: int, y: int, z: int) by (nonlinear_arith)
+    ensures
+        x < y && z > 0 ==> #[trigger] (z * x) < #[trigger] (z * y)
 {}
 
 } // verus!
--- rlsf-verified/src/relation_utils.rs
+++ rlsf-verified/src/relation_utils.rs
@@ -6,12 +6,16 @@ use vstd::seq::*;
 use vstd::set::Set;
 
 verus! {
-use vstd::relations::{transitive, connected, equivalence_relation, irreflexive};
+use vstd::relations::{transitive, equivalence_relation, irreflexive};
 
 pub open spec fn antisymmetric<T>(r: spec_fn(T, T) -> bool, eq: spec_fn(T, T) -> bool) -> bool
     recommends equivalence_relation(eq)
 {
     forall|x: T, y: T| #[trigger] r(x, y) && #[trigger] r(y, x) ==> eq(x, y)
+}
+
+pub open spec fn connected<T>(r: spec_fn(T, T) -> bool, eq: spec_fn(T, T) -> bool) -> bool {
+    forall|x: T, y: T| !eq(x, y) ==> #[trigger] r(x, y) || #[trigger] r(y, x)
 }
 
 pub open spec fn strict_total_ordering<T>(r: spec_fn(T, T) -> bool, eq: spec_fn(T, T) -> bool) -> bool
@@ -20,7 +24,7 @@ pub open spec fn strict_total_ordering<T>(r: spec_fn(T, T) -> bool, eq: spec_fn(
     &&& irreflexive(r)
     &&& antisymmetric(r, eq)
     &&& transitive(r)
-    &&& connected(r)
+    &&& connected(r, eq)
 }
 
 }
